697b8568970acb95c55eb80a4d4a73955927a326
millos910 <emontero910@gmail.com>
2023-07-25 14:11:54 -0500

    finalizado

--- src/components/UsersCard.jsx
+++ src/components/UsersCard.jsx
@@ -9,6 +9,7 @@ const UsersCard = ({ user,delteUserByid,setUpdateInfo,setisCloseForm}) => {
     }
   return (
     <article className='article__card'>
+        <br />
         <div className='principal__card'>
             <h2 className='title__card'>{`${user.first_name} ${user.last_name}`}</h2>
             <ul className='lista'>
@@ -16,14 +17,16 @@ const UsersCard = ({ user,delteUserByid,setUpdateInfo,setisCloseForm}) => {
                     <span>Email: </span>
                     <span>{user.email}</span>
                 </li>
-                <li className='segundo item'>
+                <li className='segundo_item'>
                     <span>Birthday: </span>
                     <span>{user.birthday}</span>
                 </li>
             </ul>
+            
         </div>
-        <button onClick={handleDelete}><i className='bx bxs-trash'></i></button>
-        <button onClick={handleUpdate}><i className='bx bxs-edit'></i></button>
+        <button className='delete_btn' onClick={handleDelete}><i className='bx bxs-trash'></i></button>
+        <button className='update_btn' onClick={handleUpdate}><i className='bx bxs-edit'></i></button>
+        
     </article>
   )
 }
